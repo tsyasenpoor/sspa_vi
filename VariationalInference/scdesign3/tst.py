dd311963a8df498f85b8123324fa44de1999a1e9
tsyasenpoor <tsyasinpoor@gmail.com>
2026-01-07 18:29:43 -0500

    tst

--- VariationalInference/scdesign3/tst.py
+++ VariationalInference/scdesign3/tst.py
@@ -6,4 +6,4 @@ if str(parent_dir) not in sys.path:
     sys.path.insert(0, str(parent_dir))
 from VariationalInference.scdesign3 import ScDesign3Simulator
 simulator = ScDesign3Simulator()
-result = simulator.simulate(input_file='/labs/Aguiar/SSPA_BRAY/BRay/ctrl_sspa_test/Bcell_GEX_20251201_control.h5ad', output_dir="./scdesign3_quick_test", n_cells=100,  n_genes=500, gene_selection="variable")
+result = simulator.simulate(input_file='/labs/Aguiar/SSPA_BRAY/BRay/miscc/ctrl_sspa_test/Bcell_GEX_20251201_control.h5ad', output_dir="./scdesign3_1000cells", n_cells=1000,  n_genes=500, gene_selection="variable")
